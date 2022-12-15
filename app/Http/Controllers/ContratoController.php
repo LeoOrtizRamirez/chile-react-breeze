3d1425a6a32aeeccd07c1a23a091cda80594ea39
LeoOrtizR <leo_123.rojo@hotmail.com>
2022-12-15 15:50:31 -0500

    Anadiendo back scrapping

---
+++ app/Http/Controllers/ContratoController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClasificacionContrato;
+use App\Models\Contrato;
+use App\Models\ContratistaContrato;
+use App\Models\SubCategoria;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Redirect;
+
+class ContratoController extends Controller
+{
+    
+    public function index()
+    {
+        //$contratos = Contrato::with('fuente')->get();
+
+        $contratos = Contrato::with('fuente', 'clasificaciones', 'contratistas')->get();
+        foreach ($contratos as $key => $value) {
+            $contratista = ContratistaContrato::where('id_contrato', $value->id)->first();
+            if($contratista){
+                $value->contratista =  $contratista->nombre;
+            }
+
+            $actividad_economica = ClasificacionContrato::where('id_contrato', $value->id)->first();
+            if($actividad_economica){
+                $sub_categoria = SubCategoria::find($actividad_economica->id_sub_categoria); 
+                $value->actividad_economica =  $sub_categoria->nombre;
+            }
+        }
+
+        return Inertia::render('Contrato/Index', compact('contratos'));
+    }
+
+   
+    public function create()
+    {
+        return Inertia::render('Contrato/Create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'id_user_clasificador'=>'required'
+        ]);
+
+        Contrato::create($request->all());
+        return Redirect::route('contratos.index');
+    }
+
+   
+    public function show(Contrato $contrato)
+    {
+        
+    }
+    
+    public function edit(Contrato $contrato)
+    {
+        return Inertia::render('Contrato/Edit', ['contrato', $contrato]);
+    }
+
+
+    public function update(Request $request, Contrato $contrato)
+    {
+        $contrato->update($request->all());
+        return Redirect::route('contrato.index');
+    }
+
+    
+    public function destroy(Contrato $contrato)
+    {
+        $contrato->delete();
+        return Redirect::route('contratos.index');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('nombre_completo')->nullable();
+            $table->string('identificacion')->nullable();
+            $table->integer('id_plan')->nullable();
+            $table->date('fecha_vencimiento')->nullable();
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
