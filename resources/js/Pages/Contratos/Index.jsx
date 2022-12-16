73ef73d01584c41df149b44c152bb9de5e35d6e6
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2022-12-16 13:13:03 -0500

    cargue de estilos en vista contratos,capa de autentificacion,menu de opciones . Se crea componente MenuOpciones

--- resources/js/Pages/Contratos/Index.jsx
+++ resources/js/Pages/Contratos/Index.jsx
@@ -1,69 +1,113 @@
-import React from 'react'
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import InputError from '@/Components/InputError'
-import PrimaryButton from '@/Components/PrimaryButton'
-import { useForm, Head } from '@inertiajs/inertia-react'
-import Post from '@/Components/Post'
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { useForm, Head } from "@inertiajs/inertia-react";
+import Post from "@/Components/Post";
+import MenuOpciones from "../../Components/MenuOpciones";
+import "../../../css/estilos-contratos-index.css";
 
-const Index = ({auth, contratos}) => {
+const Index = ({ auth, contratos }) => {
     const { data, setData, post, processing, reset, errors } = useForm({
-      title: '',
-      body: ''  
-    })
+        title: "",
+        body: "",
+    });
 
-    const submit = (e) => {        
-        e.preventDefault()
+    const submit = (e) => {
+        e.preventDefault();
         //console.log(data)
-        post(route('contratos.store'), {onSuccess: ()=> reset() })
-    }
+        post(route("contratos.store"), { onSuccess: () => reset() });
+    };
 
-  return (
-    <AuthenticatedLayout auth={auth}>
-        <Head title='Contratos' />
-        <div className='bg-white overflow-auto'>
-                <table className='w-full bg-white border border-indigo-600'>
-                    <thead className='bg-light text-dark' style={{backgroundColor: "#00a1c9"}}>
-                        <tr className='bg-paginador text-white uppercase text-sm leading-normal'>
-                            <th>Acciones</th>
-                            <th>Portal</th>
-                            <th>Entidad</th>
-                            <th>Objeto</th>
-                            <th>Cuantía</th>
-                            <th>Modalidad</th>
-                            <th>Número</th>
-                            <th>Estado</th>
-                            <th>F. publicación</th>
-                            <th>Ubicación</th>
-                            <th>Contratista</th>
-                            <th>Actividad económica</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-
-
-                        { contratos.map( (contrato) => (
-                            <tr key={contrato.id}>
-                                <td className='border border-gray-200 text-left py-3 px-4'>
-                                </td>
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.fuente.alias_portal} </td>    
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.entidad_contratante} </td>    
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.objeto.substr(0, 30)}... <a href="" className="text-primary">Ver más</a></td>  
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.valor > 0 ? contrato.valor.toLocaleString("co-CO") : contrato.valor_texto} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.modalidad} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.codigo_proceso} </td>    
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.estado_proceso} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.fecha_publicacion} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.ubicacion} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.contratista} </td> 
-                                <td className='border border-gray-200 text-left py-3 px-4'> {contrato.actividad_economica} </td> 
+    return (
+        <AuthenticatedLayout auth={auth}>
+            <Head title="Contratos" />
+            <div className="contenedor-contratos">
+                <MenuOpciones />
+                <div className="alto-tabla bg-white overflow-auto">
+                    <table className="w-full bg-white border tabla">
+                        <thead
+                            className="bg-light text-dark cabecera-tabla"
+                            style={{ backgroundColor: "#00a1c9" }}
+                        >
+                            <tr className="bg-paginador text-white uppercase leading-normal">
+                                <th>Acciones</th>
+                                <th>Portal</th>
+                                <th>Entidad</th>
+                                <th>Objeto</th>
+                                <th>Cuantía</th>
+                                <th>Modalidad</th>
+                                <th>Número</th>
+                                <th>Estado</th>
+                                <th>Publicada</th>
+                                <th>Ubicación</th>
+                                <th>Contratista</th>
+                                <th>Actividad económica</th>
                             </tr>
-                        )) }
-                    </tbody>
-                </table>
-        </div>
-    </AuthenticatedLayout>
-  )
-}
+                        </thead>
+                        <tbody>
+                            {contratos.map((contrato) => (
+                                <tr key={contrato.id}>
+                                    <td className="border border-gray-200 text-left py-3 px-4"></td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.fuente.alias_portal}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.entidad_contratante}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.objeto.substr(0, 30)}...{" "}
+                                        <a href="" className="text-primary">
+                                            Ver más
+                                        </a>
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.valor > 0
+                                            ? contrato.valor.toLocaleString(
+                                                  "co-CO"
+                                              )
+                                            : contrato.valor_texto}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.modalidad}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.codigo_proceso}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.estado_proceso}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.fecha_publicacion}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.ubicacion}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.contratista}{" "}
+                                    </td>
+                                    <td className="border border-gray-200 text-left py-3 px-4">
+                                        {" "}
+                                        {contrato.actividad_economica}{" "}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+};
 
-export default Index
+export default Index;
