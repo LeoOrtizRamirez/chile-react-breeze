a19b3849c706eb95c233b07af131c0816bdda209
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2022-12-26 12:34:34 -0500

    cargue logo pagina de inicio

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,13 +1,20 @@
-import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from '@inertiajs/inertia-react';
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import ApplicationLogoLici from "@/Components/ApplicationLogoLici";
+import { Link } from "@inertiajs/inertia-react";
 import "../../css/estilos-capa-autentificacion.css";
 
 export default function Guest({ children }) {
     return (
-        <div style={{ backgroundColor : '#00a1c9' }} className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+        <div
+            // style={{ backgroundColor: "#00a1c9" }}
+            className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-200"
+        >
+            <div className="contenedor-logos">
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                </Link>
+                <Link href="/">
+                    <ApplicationLogoLici className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
             </div>
 
