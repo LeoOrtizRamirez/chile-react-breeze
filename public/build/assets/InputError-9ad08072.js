ea4df33b25de19c8201be563e85cedfe5dde2ec0
LeoOrtizR <leo_123.rojo@hotmail.com>
2022-12-28 14:47:42 -0500

    Deploy

--- public/build/assets/InputError-9ad08072.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputError-9ad08072.js
-import{a as n}from"./app-9bac296b.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
-========
-import{a as n}from"./app-9326943b.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
->>>>>>>> c3c4bcea26394ef1e3060e39c3f7e1d941511e29:public/build/assets/InputError-640872bf.js
--- public/build/assets/InputLabel-4291933f.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/InputLabel-bb70a09c.js
-import{a as m}from"./app-9bac296b.js";function l({forInput:e,value:t,className:r,children:a}){return m("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+r,children:t||a})}export{l as I};
-========
-import{a as m}from"./app-9326943b.js";function l({forInput:e,value:t,className:r,children:a}){return m("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+r,children:t||a})}export{l as I};
->>>>>>>> c3c4bcea26394ef1e3060e39c3f7e1d941511e29:public/build/assets/InputLabel-4291933f.js
