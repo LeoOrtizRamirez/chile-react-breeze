deb33abb2d38ef68023bc1fc010b4c9c4d640bbd
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2023-01-04 17:15:27 -0500

    avances banner monitoreamos

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -8,35 +8,8 @@ export default function Welcome(props) {
 
     return (
         <>
-            {/* <Head title="Welcome" />
-            <div className="fixed top-0 right-0 px-6 py-4 sm:block">
-                {props.auth.user ? (
-                    <Link
-                        href={route("dashboard")}
-                        className="text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Dashboard
-                    </Link>
-                ) : (
-                    <>
-                        <Link
-                            href={route("login")}
-                            className="text-sm text-gray-700 dark:text-gray-500 underline"
-                        >
-                            Log in
-                        </Link>
-
-                        <Link
-                            href={route("register")}
-                            className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                        >
-                            Register
-                        </Link>
-                    </>
-                )}
-            </div> */}
-            <Banner procesos={props.contratos}></Banner>
-            <BannerMonitoreamos />
+            <Banner/>
+            <BannerMonitoreamos/>
             <Footer />
         </>
     );
