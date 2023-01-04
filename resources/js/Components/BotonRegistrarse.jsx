0a5bcbc6971422aac464e25241c1ccb8073f9c90
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-01-04 15:29:06 -0500

    Crear componente BotonRegistrarse

---
+++ resources/js/Components/BotonRegistrarse.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BotonRegistrarse = () => {
+    return (
+        <div className="col-12 text-center"><a data-v-26cd45b0="" id="CTAButton" href="/register" className="cta-button shadow-button banner-video-paises__button success">
+            <div data-v-26cd45b0="" className="cta-content"><span data-v-26cd45b0="" className="cta-button-text">Regístrate y obtén ¡30 días gratis!</span>
+                <div data-v-26cd45b0="" className="cta-button-icon-content">
+                    <span data-v-26cd45b0="" className="cta-button-icon-content-hidden-text">Regístrate y obtén 30 días de servicio</span>
+                    <span data-v-26cd45b0="" class="cta-button-icon-content-icon icon-Right"></span>
+                </div>
+            </div></a>
+        </div>
+    )
+}
+
+export default BotonRegistrarse
