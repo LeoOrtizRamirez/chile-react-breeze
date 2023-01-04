1cd57aa2d0084441aea46a75f1396378a23640f6
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2023-01-04 15:18:06 -0500

    Ajustes Header

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -7,33 +7,6 @@ export default function Welcome(props) {
         <>
             <Head title="Welcome" />
             <Header user={props}></Header>
-            <div className="fixed top-0 right-0 px-6 py-4 sm:block">
-                {props.auth.user ? (
-                    <Link
-                        href={route("dashboard")}
-                        className="text-sm text-gray-700 dark:text-gray-500 underline"
-                    >
-                        Dashboard
-                    </Link>
-                ) : (
-                    <>
-                        <Link
-                            href={route("login")}
-                            className="text-sm text-gray-700 dark:text-gray-500 underline"
-                        >
-                            Log in
-                        </Link>
-
-                        <Link
-                            href={route("register")}
-                            className="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"
-                        >
-                            Register
-                        </Link>
-                    </>
-                )}
-            </div>
-
             <Banner></Banner>
             {/* Componentes */}
             <Footer />
