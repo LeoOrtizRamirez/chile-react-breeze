5dd05049ad969fe8f4c0e1ea6c43c583e57fe094
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2023-01-04 17:08:58 -0500

    Componente Carousel Imagenes

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,8 @@
 import { Link, Head } from "@inertiajs/inertia-react";
-import Banner from "@/Components/Banner";import { Footer } from "../Components/Footer/Footer";
+import Banner from "@/Components/Banner"; import { Footer } from "../Components/Footer/Footer";
+import "../../css/estilos-carousel-publica.css";
+import BotonRegistrarse from '@/Components/BotonRegistrarse';
+import CarouselImagenes from '@/Components/CarouselImagenes';
 
 import Header from "@/Layouts/HeaderPublica";
 export default function Welcome(props) {
@@ -10,7 +13,22 @@ export default function Welcome(props) {
             <Header user={props}></Header>
             <Banner procesos={props.contratos}></Banner>
             {/* Componentes */}
-            <Footer />
+
+            <div class="grid grid-cols-12 my-2 p-5">
+                <div class="col-span-6 customers-cta">
+                    <div class="contenta-a">
+                        <span class="customers-cta-since-year">
+                            Desde 2004
+                        </span>
+
+                        <span class="customers-cta-count-statistics">
+                            Más de 6000 clientes en Colombia y el mundo
+                        </span>
+                    </div>
+                    <BotonRegistrarse/>
+                </div>
+                <CarouselImagenes />
+            </div>
         </>
     );
-}
+}   
