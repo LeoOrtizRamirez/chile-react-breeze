fcb2cfb9e3b2e686ac64a46c595e39fcb2735b9b
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-01-04 16:34:52 -0500

    Ajustar estilos

--- resources/js/Components/BotonRegistrarse.jsx
+++ resources/js/Components/BotonRegistrarse.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
+import "../../css/estilos-boton-registrarse.css";
 const BotonRegistrarse = () => {
     return (
-        <div className="col-12 text-center"><a data-v-26cd45b0="" id="CTAButton" href="/register" className="cta-button shadow-button banner-video-paises__button success">
-            <div data-v-26cd45b0="" className="cta-content"><span data-v-26cd45b0="" className="cta-button-text">Regístrate y obtén ¡30 días gratis!</span>
-                <div data-v-26cd45b0="" className="cta-button-icon-content">
-                    <span data-v-26cd45b0="" className="cta-button-icon-content-hidden-text">Regístrate y obtén 30 días de servicio</span>
-                    <span data-v-26cd45b0="" class="cta-button-icon-content-icon icon-Right"></span>
+        <div className="col-12 text-center">
+            <a href="/register" className="td-none cta-button shadow-button banner-video-paises__button success">
+                <div className="cta-content">
+                    <span className="cta-button-text">Regístrate y obtén ¡30 días gratis!</span>
+                    <div className="cta-button-icon-content">
+                        <span className="cta-button-icon-content-hidden-text">Regístrate y obtén 30 días de servicio</span>
+                        <span class="cta-button-icon-content-icon icon-Right"></span>
+                    </div>
                 </div>
-            </div></a>
+            </a>
         </div>
     )
 }
