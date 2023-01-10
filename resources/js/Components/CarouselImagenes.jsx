1bd96f0104f8d5694ed684cc16ececf52fb092ba
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2023-01-10 08:36:20 -0500

    Se ajustan las rutas de las imagenes y en planes

--- resources/js/Components/CarouselImagenes.jsx
+++ resources/js/Components/CarouselImagenes.jsx
@@ -22,50 +22,50 @@ const CarouselImagenes = () => {
                         <img src="/public/images/carousel/Davivienda.webp" alt="Davivienda" />
                         <img src="/public/images/carousel/Dobleclick.webp" alt="Dobleclick" />
                         <img src="/public/images/carousel/Doxa.webp" alt="Doxa" />
-                        <img src="/public/images/carousel/Egis.webp" alt="Egis" /> 
+                        <img src="/public/images/carousel/Egis.webp" alt="Egis" />
                     </div>
 
 
                     <div className="carousel-row">
-                        <img src="/images/carousel/Elbosque.webp" alt="El bosque" />
-                        <img src="/images/carousel/Elespectador.webp" alt="El espectador" />
-                        <img src="/images/carousel/EPM.webp" alt="EPM" />
-                        <img src="/images/carousel/Epson.webp" alt="Epson" />
-                        <img src="/images/carousel/FaberCastell.webp" alt="Faber Castell" />
-                        <img src="/images/carousel/Fiduagraria.webp" alt="Fiduagraria" />
-                        <img src="/images/carousel/GBarco.webp" alt="GBarco" />
-                        <img src="/images/carousel/GCSI.webp" alt="GCSI" />
-                        <img src="/images/carousel/Global SAT.webp" alt="Global SAT" />
-                        <img src="/images/carousel/Hospitecnica.webp" alt="Hospitécnica" />
-                        <img src="/images/carousel/Icontec.webp" alt="Icontec" />
-                        <img src="/images/carousel/Indra.webp" alt="Indra" />
-                        <img src="/images/carousel/Interactuar.webp" alt="Interactuar" />
-                        <img src="/images/carousel/IT Synergy.webp" alt="IT Synergy" />
-                        <img src="/images/carousel/Itaca.webp" alt="Itaca" />
-                        <img src="/images/carousel/Javeriana.webp" alt="Javeriana" />
-                        <img src="/images/carousel/Laurbe.webp" alt="La urbe" />
-                        <img src="/images/carousel/Learn.webp" alt="Learn" />
+                        <img src="/public/images/carousel/Elbosque.webp" alt="El bosque" />
+                        <img src="/public/images/carousel/Elespectador.webp" alt="El espectador" />
+                        <img src="/public/images/carousel/EPM.webp" alt="EPM" />
+                        <img src="/public/images/carousel/Epson.webp" alt="Epson" />
+                        <img src="/public/images/carousel/FaberCastell.webp" alt="Faber Castell" />
+                        <img src="/public/images/carousel/Fiduagraria.webp" alt="Fiduagraria" />
+                        <img src="/public/images/carousel/GBarco.webp" alt="GBarco" />
+                        <img src="/public/images/carousel/GCSI.webp" alt="GCSI" />
+                        <img src="/public/images/carousel/Global SAT.webp" alt="Global SAT" />
+                        <img src="/public/images/carousel/Hospitecnica.webp" alt="Hospitécnica" />
+                        <img src="/public/images/carousel/Icontec.webp" alt="Icontec" />
+                        <img src="/public/images/carousel/Indra.webp" alt="Indra" />
+                        <img src="/public/images/carousel/Interactuar.webp" alt="Interactuar" />
+                        <img src="/public/images/carousel/IT Synergy.webp" alt="IT Synergy" />
+                        <img src="/public/images/carousel/Itaca.webp" alt="Itaca" />
+                        <img src="/public/images/carousel/Javeriana.webp" alt="Javeriana" />
+                        <img src="/public/images/carousel/Laurbe.webp" alt="La urbe" />
+                        <img src="/public/images/carousel/Learn.webp" alt="Learn" />
                     </div>
                     <div className="carousel-row">
-                        <img src="/images/carousel/Minepro.webp" alt="Minepro" />
-                        <img src="/images/carousel/Multicomputo.webp" alt="Multicomputo" />
-                        <img src="/images/carousel/Nexys.webp" alt="Nexys" />
-                        <img src="/images/carousel/Oracle.webp" alt="Oracle" />
-                        <img src="/images/carousel/Panamericana.webp" alt="Panamericana" />
-                        <img src="/images/carousel/ParqueSoft.webp" alt="Parque Soft" />
-                        <img src="/images/carousel/Philips.webp" alt="Philips" />
-                        <img src="/images/carousel/Praxair.webp" alt="Praxair" />
-                        <img src="/images/carousel/QHSE.webp" alt="QHSE" />
-                        <img src="/images/carousel/Rentainer.webp" alt="Rentainer" />
-                        <img src="/images/carousel/RUA.webp" alt="RUA" />
-                        <img src="/images/carousel/Sergioarboleda.webp" alt="Sergio arboleda" />
-                        <img src="/images/carousel/Servientrega.webp" alt="Servientrega" />
-                        <img src="/images/carousel/Siemens.webp" alt="Siemens" />
-                        <img src="/images/carousel/Sodexo.webp" alt="Sodexo" />
-                        <img src="/images/carousel/Telefonica.webp" alt="Telefonica" />
-                        <img src="/images/carousel/Thomas.webp" alt="Thomas" />
-                        <img src="/images/carousel/Ticsa.webp" alt="Ticsa" />
-                        <img src="/images/carousel/Tubosa.webp" alt="Tubosa" />
+                        <img src="/public/images/carousel/Minepro.webp" alt="Minepro" />
+                        <img src="/public/images/carousel/Multicomputo.webp" alt="Multicomputo" />
+                        <img src="/public/images/carousel/Nexys.webp" alt="Nexys" />
+                        <img src="/public/images/carousel/Oracle.webp" alt="Oracle" />
+                        <img src="/public/images/carousel/Panamericana.webp" alt="Panamericana" />
+                        <img src="/public/images/carousel/ParqueSoft.webp" alt="Parque Soft" />
+                        <img src="/public/images/carousel/Philips.webp" alt="Philips" />
+                        <img src="/public/images/carousel/Praxair.webp" alt="Praxair" />
+                        <img src="/public/images/carousel/QHSE.webp" alt="QHSE" />
+                        <img src="/public/images/carousel/Rentainer.webp" alt="Rentainer" />
+                        <img src="/public/images/carousel/RUA.webp" alt="RUA" />
+                        <img src="/public/images/carousel/Sergioarboleda.webp" alt="Sergio arboleda" />
+                        <img src="/public/images/carousel/Servientrega.webp" alt="Servientrega" />
+                        <img src="/public/images/carousel/Siemens.webp" alt="Siemens" />
+                        <img src="/public/images/carousel/Sodexo.webp" alt="Sodexo" />
+                        <img src="/public/images/carousel/Telefonica.webp" alt="Telefonica" />
+                        <img src="/public/images/carousel/Thomas.webp" alt="Thomas" />
+                        <img src="/public/images/carousel/Ticsa.webp" alt="Ticsa" />
+                        <img src="/public/images/carousel/Tubosa.webp" alt="Tubosa" />
                     </div>
 
                 </div>
