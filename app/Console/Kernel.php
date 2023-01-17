66672adc86668be3f9a6d3315f01926c24228f74
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-01-17 16:01:06 -0500

    Se configuro la kernel del DailyScrapping para ejecutar el cronjob cada 2 horas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:DailyScrapping')->everyMinute();
+        //$schedule->command('command:DailyScrapping')->everyMinute();
+        $schedule->command('command:DailyScrapping')->everyTwoHours();
     }
 
     /**
