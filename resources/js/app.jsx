e22cb1d54f975622918e0c6c0a8de748bf61b263
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-01-25 13:40:42 -0500

    Agregar favicon y cambiar titulo de las pagimas

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,10 +7,11 @@ import { InertiaProgress } from "@inertiajs/progress";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 
 const appName =
-    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
+    window.document.getElementsByTagName("title")[0]?.innerText || "Licitaciones en Chile";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    /* title: (title) => `${title} - ${appName}`, */
+    title: (title) => `${appName}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.jsx`,
