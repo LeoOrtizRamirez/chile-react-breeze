b81db2aa69915274d6595dd4a27a84a78eeff138
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2023-01-26 07:57:45 -0500

    Ajustes Header

---
+++ resources/js/Layouts/script.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', function() {
+
+    if(window.location.href.indexOf("/funcionalidades") > -1) {
+        document.querySelectorAll("a[href='/funcionalidades']")[0].style.color="#00A1C9";
+    }else{
+        if(window.location.href.indexOf("/planes") > -1) {
+            document.querySelectorAll("a[href='/chile/planes']")[0].style.color="#00A1C9";
+        }else{
+            if(window.location.href.indexOf("/nosotros") > -1) {
+                document.querySelectorAll("a[href='/nosotros']")[0].style.color="#00A1C9";
+            }else{
+                if(window.location.href.indexOf("/contacto") > -1) {
+                    document.querySelectorAll("a[href='/contacto']")[0].style.color="#00A1C9";
+                }  
+            }
+        }
+    }
+});
