a5df94a1942f4c4b0653e020ec7115c6306923da
giraldo.mauricio97@gmail.com <giraldo.mauricio97@gmail.com>
2023-01-30 10:40:16 -0500

    Ajustes estilos en paginas

--- resources/js/Components/BotonRegistrarse.jsx
+++ resources/js/Components/BotonRegistrarse.jsx
@@ -3,13 +3,13 @@ import "../../css/estilos-boton-registrarse.css";
 const BotonRegistrarse = (props) => {
     return (
         <div className="col-12">
-            <a href="/register" className={`td-none cta-button bg-${props.color} shadow-button banner-video-paises__button hover:bg-[${props.colorHover}]`}>
+            <a href="/register" className={`td-none cta-button-register bg-${props.color} shadow-button-register  hover:bg-[${props.colorHover}]`}>
     
-                <div className="cta-content">
-                    <span className="cta-button-text">{props.texto}</span>
-                    <div className={`cta-button-icon-content`}>
-                        <span className={`cta-button-icon-content-hidden-text `}>{props.textoHover}</span>
-                        <span className="cta-button-icon-content-icon icon-Right"></span>
+                <div className="cta-content-register">
+                    <span className="cta-button-text-register">{props.texto}</span>
+                    <div className={`cta-button-icon-content-register`}>
+                        <span className={`cta-button-icon-content-register-hidden-text `}>{props.textoHover}</span>
+                        <span className="cta-button-icon-content-register-icon icon-Right"></span>
                     </div>
                 </div>
             </a>
