3875acb8c68c1b62c4f628f8fcf3da1d5286c2e3
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2023-02-06 09:33:37 -0500

    ajustes sub menu build

---
+++ public/build/assets/Crear-315bde63.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-6d4d7861.js";const t=()=>r("div",{children:"VISTA CREAR"});export{t as default};
--- public/build/assets/Editar-5041c866.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./app-efc02201.js";const t=()=>r("div",{children:"VISTA EDITAR"});export{t as default};
