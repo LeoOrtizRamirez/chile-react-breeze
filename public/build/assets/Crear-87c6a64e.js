3b81710ea0d5b530c808847a3817727a44b0af5c
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2023-02-07 09:21:09 -0500

    build

---
+++ public/build/assets/Crear-87c6a64e.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-9a505d52.js";const t=()=>r("div",{children:"VISTA CREAR"});export{t as default};
---
+++ public/build/assets/Editar-f02b230b.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-9a505d52.js";const t=()=>r("div",{children:"VISTA EDITAR"});export{t as default};
