ff8ad5d2e7b768568c51752e252c60fdd2f45a57
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-02-09 09:31:42 -0500

    Agregar validaciones front en los campos

--- resources/js/Pages/ActividadesEconomicas/Crear.jsx
+++ resources/js/Pages/ActividadesEconomicas/Crear.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { useForm, Head } from "@inertiajs/inertia-react";
 import MenuOpciones from "../../Components/Menu_opciones/MenuOpciones";
 import './Crear.css';
 
@@ -14,9 +13,15 @@ import "../../../css/font-unicolor.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 
+/*Form */
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useForm, Head } from "@inertiajs/inertia-react";
+/*Form */
+
 const Crear = ({ auth, actividades_economicas, solo_sectores }) => {
-    const [showToast, setShowToast] = useState(false);
-    const [toastMessage, setToastMessage] = useState("");
+    const [showToast, setShowToast] = useState(false)
+    const [toastMessage, setToastMessage] = useState("")
     const { data, setData, post, processing, reset, errors } = useForm({
         id: "",
         nombre: "",
@@ -24,20 +29,48 @@ const Crear = ({ auth, actividades_economicas, solo_sectores }) => {
         segmento: "",
         tipo_categoria: 1,
     });
+    const [fakeSectores, setFakeSectores] = useState(actividades_economicas)
+    const [sectores, setSectores] = useState(solo_sectores)
+    const [segmentos, setSegmentos] = useState([])
+
+    const [disabledClass, setDisabledClass] = useState("disabled")
+    const [validated, setValidated] = useState(false)
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        setValidated(true);
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+            post(route("actividades-economicas.store"), { onSuccess: () => reset() });
+        }
+
+    };
+
+    const onHandleChange = (event) => {
+        console.log(event.target);
+        setData(
+            event.target.name,
+            event.target.type === "checkbox"
+                ? event.target.checked
+                : event.target.value
+        );
+    };
+
     useEffect(() => {
         if (Object.entries(errors).length > 0) {
             var responses = Object.values(errors)
             var message = '';
-            {responses.map((response) => (
-                message += response
-            ))}
+            {
+                responses.map((response) => (
+                    message += response
+                ))
+            }
             setToastMessage(message)
             setShowToast(true)
         }
     }, [errors])
-    const [fakeSectores, setFakeSectores] = useState(actividades_economicas);
-    const [sectores, setSectores] = useState(solo_sectores);
-    const [segmentos, setSegmentos] = useState([])
 
     const getSegmentos = (e) => {
         setData("sector", e.target.value)
@@ -83,78 +116,105 @@ const Crear = ({ auth, actividades_economicas, solo_sectores }) => {
                 <div className="bg-white overflow-auto w-full text-center margen-superior">
                     <h2 className="name_section_app">Crear Actividad económica</h2>
                     <div className="container mt-4">
-                        <form onSubmit={submit}>
-                            <div className="row">
+
+                        <Form
+                            id="form"
+                            name="form"
+                            noValidate
+                            validated={validated}
+                            onSubmit={handleSubmit}
+                        >
+                            <Form.Group className="row">
                                 <div className="col-12 col-sm-4">
-                                    <label htmlFor="">Seleccione el sector</label>
+                                    <Form.Label
+                                        htmlFor=""
+                                        for="validationInput"
+                                        className="bloque__registro-form-title-label"
+                                    >
+                                        Seleccione el sector
+                                    </Form.Label>
                                 </div>
                                 <div className="col-12 col-sm-8">
-                                    <select name="sector" className="sector" id="" onChange={getSegmentos}>
+                                    <Form.Select name="sector" className="sector" id="" onChange={getSegmentos}>
                                         <option value="">Selecciona un Sector</option>
                                         {sectores.map((sector) => (
                                             <option value={sector.id}>{sector.nombre}</option>
                                         ))}
-
-                                    </select>
+                                    </Form.Select>
                                 </div>
-                            </div>
-                            <div className="row mt-4">
+                            </Form.Group>
+                            <Form.Group className="row mt-3">
                                 <div className="col-12 col-sm-4">
-                                    <label htmlFor="">Seleccione el segmento</label>
+                                    <Form.Label
+                                        htmlFor=""
+                                        for="validationInput"
+                                        className="bloque__registro-form-title-label"
+                                    >
+                                        Seleccione el segmento
+                                    </Form.Label>
                                 </div>
                                 <div className="col-12 col-sm-8">
-                                    <select name="segmento" className="segmento" id="" onChange={(e) => setData("segmento", e.target.value)}>
+                                    <Form.Select name="segmento" className="segmento" id="" onChange={(e) => setData("segmento", e.target.value)}>
                                         <option value="">Selecciona un Segmento</option>
                                         {segmentos.map((segmento) => (
                                             <option value={segmento.id}>{segmento.nombre}</option>
                                         ))}
-
-                                    </select>
+                                    </Form.Select>
                                 </div>
-                            </div>
-                            <div className="row mt-4">
+                            </Form.Group>
+                            <Form.Group className="row mt-3">
                                 <div className="col-12 col-sm-4">
-                                    <label htmlFor="">Nombre Actividad económica</label>
+                                    <Form.Label
+                                        htmlFor=""
+                                        for="validationInput"
+                                        className="bloque__registro-form-title-label"
+                                    >
+                                        Nombre Actividad económica
+                                    </Form.Label>
                                 </div>
                                 <div className="col-12 col-sm-8">
-                                    <input
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Nombre Actividad económica"
                                         value={data.nombre}
                                         onChange={(e) =>
                                             setData("nombre", e.target.value)
                                         }
-                                        type="text"
-                                        placeholder="Nombre"
                                         className="actividad-economica"
+                                        required
                                     />
                                 </div>
-                            </div>
-                            <div className="row mt-4">
+                            </Form.Group>
+                            <Form.Group className="row mt-3">
                                 <div className="col-12 col-sm-4">
-                                    <label htmlFor="">Código de verificación</label>
+                                    <Form.Label
+                                        htmlFor=""
+                                        for="validationInput"
+                                        className="bloque__registro-form-title-label"
+                                    >
+                                        Código de verificación
+                                    </Form.Label>
                                 </div>
                                 <div className="col-12 col-sm-8">
-                                    <input
+                                    <Form.Control
+                                        type="number"
+                                        placeholder="Código de verificación"
                                         value={data.id}
                                         onChange={(e) =>
                                             setData("id", e.target.value)
                                         }
-                                        type="number"
-                                        placeholder="Código de verificación"
                                         className="actividad-economica"
+                                        required
                                     />
                                 </div>
-                            </div>
-                            <div className="row mt-4">
-                                <div className="col-4"></div>
-                                <div className="col-4">
-                                    <button type="submit" className="btn btnRadius btn-new-blue mr-2">Crear</button>
-                                    <a href={route("actividades-economicas.index")} className="btn btnRadius btn-new-red ml-2">
-                                        Cancelar
-                                    </a>
-                                </div>
-                                <div className="col-4"></div>
-                            </div>
-                        </form>
+                            </Form.Group>
+                            <Form.Group className="mt-4">
+                                <button type="submit" className={`btn btnRadius btn-new-blue mr-2`}>Crear</button>
+                                <a href={route("actividades-economicas.index")} className="btn btnRadius btn-new-red ml-2">
+                                    Cancelar
+                                </a>
+                            </Form.Group>
+                        </Form>
                     </div>
                 </div>
             </div>
