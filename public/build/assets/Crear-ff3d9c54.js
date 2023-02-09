097999481c0ad69f13b01fb2db98999717be44d3
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-02-08 23:12:49 -0500

    Desplegar en servidor

---
+++ public/build/assets/Crear-ff3d9c54.js
@@ -0,0 +1,1 @@
+import{a as r}from"./app-6ba39544.js";const t=()=>r("div",{children:"VISTA CREAR"});export{t as default};
--- public/build/assets/Editar-b233db55.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./app-8d306641.js";const t=()=>r("div",{children:"VISTA EDITAR"});export{t as default};
