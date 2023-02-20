b2597156c3700e02c6e068e583815968cdf983dd
stevgomezt <97990647+stevgomezt@users.noreply.github.com>
2023-02-20 14:56:12 -0500

    build

--- public/build/assets/useIsomorphicEffect-fe3abe4a.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./app-67a65f7d.js";var t=typeof global<"u"&&global.navigator&&global.navigator.product==="ReactNative",a=typeof document<"u";const r=a||t?e.exports.useLayoutEffect:e.exports.useEffect;export{r as u};
