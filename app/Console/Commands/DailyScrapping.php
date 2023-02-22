abc63f962c93cbd92aaf75143689142b34b3f83f
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-02-22 10:09:33 -0500

    Organizar validacion de estado de proceso

--- app/Console/Commands/DailyScrapping.php
+++ app/Console/Commands/DailyScrapping.php
@@ -94,7 +94,7 @@ class DailyScrapping extends Command
 
             //1. Extraccion de informacion de encabezados.
             $dataEncabezado = array();
-            $crawler->filter(".responsive-resultado")->each(function ($node) use (&$dataEncabezado) {
+            $crawler->filter(".responsive-resultado")->each(function ($node) use (&$dataEncabezado, &$pagina, &$num_paginas) {
                 //Filtrar datos
 
                 //construccion id_licitacion
@@ -166,7 +166,7 @@ class DailyScrapping extends Command
                     echo "El contrato ya existe...\n";
                 }else{
                     $model->save();
-                    echo "Guardando Concurso\n";
+                    echo "Guardando Contrato - pagina: " . $pagina . " de: ".$num_paginas. "\n";
                     $this->guardarDetalle($model, $contratista_nombre);
                     //Fin - Actividad economica
 
@@ -212,38 +212,21 @@ class DailyScrapping extends Command
         $crawlerDetalle = $this->getClient()->request('GET', $model->link);
         $model->modalidad = $this->textValidation($crawlerDetalle->filter('#lblFicha1Tipo'));
         $model->ubicacion = $this->textValidation($crawlerDetalle->filter('#lblFicha2Region'));
-        $estado_proceso = $this->textValidation($crawlerDetalle->filter('#imgEstado'), '#imgEstado', 'src');
+        $estado_proceso_fuente = $this->textValidation($crawlerDetalle->filter('#imgEstado'), '#imgEstado', 'src');
 
-        while($estado_proceso == ""){
-            $crawlerDetalle = $this->getClient()->request('GET', $model->link);
-            $estado_proceso = $this->textValidation($crawlerDetalle->filter('#imgEstado'), '#imgEstado', 'src');
-            switch ($estado_proceso) {
-                case '../../Includes/images/FichaLight/iconos_estados/publicadas.png':
-                    $model->estado_proceso =  "Publicada";
-                    break;
-                case '../../Includes/images/FichaLight/iconos_estados/cerrada.png':
-                    $model->estado_proceso =  "Cerrada";
-                    break;
-                case '../../Includes/images/FichaLight/iconos_estados/desierta.png':
-                    $model->estado_proceso =  "Desierta";
-                    break;
-                case '../../Includes/images/FichaLight/iconos_estados/adjudicada.png':
-                    $model->estado_proceso =  "Adjudicada";
-                    break;
-                case '../../Includes/images/FichaLight/iconos_estados/revocada.png':
-                    $model->estado_proceso =  "Revocada";
-                    break;
-                case '../../Includes/images/FichaLight/iconos_estados/suspendida.png':
-                    $model->estado_proceso =  "Suspendida";
-                    break;
-                default:
-                    # code...
-                    break;
-            }
+        if($estado_proceso_fuente != ""){
+            $estado_proceso = $this->getEstadoProceso($estado_proceso_fuente);
+        }else{
+            do {
+                $crawlerDetalle = $this->getClient()->request('GET', $model->link);
+                $estado_proceso_fuente = $this->textValidation($crawlerDetalle->filter('#imgEstado'), '#imgEstado', 'src');
+                $estado_proceso = $this->getEstadoProceso($estado_proceso_fuente);
+            } while ($estado_proceso_fuente == "");
         }
-
+        $model->estado_proceso = $estado_proceso;
+        echo "Estado:" . $estado_proceso. "\n";
         $model->save();
-        echo "Guardando Detalle del Concurso\n";
+        echo "Guardando Detalle del Contrato\n";
 
         $contratista = new ContratistaContrato;
         $contratista->nombre = $contratista_nombre;
@@ -274,6 +257,34 @@ class DailyScrapping extends Command
         $clasificacion_contrato->save();
         echo "Guardando ClasificacionContrato\n\n";
         
+    }
+
+    public function getEstadoProceso($img){
+        $estado_proceso = $img;
+        switch ($img) {
+            case '../../Includes/images/FichaLight/iconos_estados/publicadas.png':
+                $estado_proceso =  "Publicada";
+                break;
+            case '../../Includes/images/FichaLight/iconos_estados/cerrada.png':
+                $estado_proceso =  "Cerrada";
+                break;
+            case '../../Includes/images/FichaLight/iconos_estados/desierta.png':
+                $estado_proceso =  "Desierta";
+                break;
+            case '../../Includes/images/FichaLight/iconos_estados/adjudicada.png':
+                $estado_proceso =  "Adjudicada";
+                break;
+            case '../../Includes/images/FichaLight/iconos_estados/revocada.png':
+                $estado_proceso =  "Revocada";
+                break;
+            case '../../Includes/images/FichaLight/iconos_estados/suspendida.png':
+                $estado_proceso =  "Suspendida";
+                break;
+            default:
+                $estado_proceso =  "Default";
+                break;
+        }
+        return $estado_proceso;
     }
 
     public function getClient()
