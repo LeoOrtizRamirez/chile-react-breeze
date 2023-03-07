7fadae2a9af9228308e9f22d87ed5f898c05f75a
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-03-07 08:31:35 -0500

    Desplegar en servidor

--- public/build/assets/useIsomorphicEffect-b7d052cb.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./app-33dbc871.js";var t=typeof global<"u"&&global.navigator&&global.navigator.product==="ReactNative",a=typeof document<"u";const r=a||t?e.exports.useLayoutEffect:e.exports.useEffect;export{r as u};
