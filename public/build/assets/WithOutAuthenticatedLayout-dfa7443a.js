40735577a606ceccaddd22e6e0f7f229e85ac1c8
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-05-02 11:47:05 -0500

    Desplegar en servidor

---
+++ public/build/assets/WithOutAuthenticatedLayout-dfa7443a.js
@@ -0,0 +1,1 @@
+import{r as o,j as s,F as i,a as t}from"./app-9cb9f483.js";/* empty css                      *//* empty css              */import{H as n,F as c}from"./Footer-42623e9b.js";function h({auth:e,header:u,children:r,page:a}){return o.exports.useState(!1),s(i,{children:[t(n,{user:e}),t("div",{id:a,children:r}),t(c,{})]})}export{h as A};
