d777488a0e28905d65c75f7c792954628a2ec928
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-06-01 07:13:44 -0500

    Desplegar en servidor

---
+++ public/build/assets/InputLabel-c1cb2070.js
@@ -0,0 +1,1 @@
+import{a as m}from"./app-2bb77d14.js";function l({forInput:e,value:t,className:r,children:a}){return m("label",{htmlFor:e,className:"block font-medium text-sm text-gray-700 "+r,children:t||a})}export{l as I};
