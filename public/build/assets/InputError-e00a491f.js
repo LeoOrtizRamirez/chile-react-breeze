d2e4478bcbce49658a170cdb2ae978d8ff11b804
LeoOrtizR <leo_123.rojo@hotmail.com>
2023-06-13 13:29:17 -0500

    Desplegar en servior

---
+++ public/build/assets/InputError-e00a491f.js
@@ -0,0 +1,1 @@
+import{a as n}from"./app-e2dbf1db.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
--- public/build/assets/WithOutAuthenticatedLayout-8f0c1e36.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as s,F as i,a as t}from"./app-5fc30145.js";/* empty css                      *//* empty css              */import{H as n,F as c}from"./Footer-2d021859.js";function h({auth:e,header:u,children:r,page:a}){return o.exports.useState(!1),s(i,{children:[t(n,{user:e}),t("div",{id:a,children:r}),t(c,{})]})}export{h as A};
